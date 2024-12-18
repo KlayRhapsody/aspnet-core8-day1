07929fbcdff54908f21868cf9dc4285f9a2d38f4
klayhung <klayhung@gmail.com>
2024-12-18 17:30:48 +0800

    新增 Mylib.Share 套件參考，並在 ConsoleApp 中使用 Student 類別，新增 nuget.config 檔案

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Mylib.Share;
+
+var student = new Student("John", 20);
+Console.WriteLine($"{student.Name} is {student.Age} years old.");
--- Mylib.Share/Class1.cs
+++ Mylib.Share/Class1.cs
@@ -1,6 +1,13 @@
 ﻿namespace Mylib.Share;
 
-public class Class1
+public class Student
 {
+    public string Name { get; set; }
+    public int Age { get; set; }
 
+    public Student(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
 }
